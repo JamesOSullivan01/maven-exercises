f320db40032f24e637ce150191b6446c18300672
James OSullivan <james.a.osullivan01@gmail.com>
2023-07-07 11:41:06 -0500

    Completed dependency exercise.

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,9 +1,28 @@
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
-        String world = "world";
-        System.out.println(StringUtils.prependIfMissing(world, "Hello, "));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter something: ");
+        String userInput = scanner.nextLine();
+        System.out.println("You entered: " + userInput);
+
+        if (StringUtils.isNumeric(userInput) == false) {
+            System.out.println(userInput + " is not a number");
+        }
+
+        System.out.println("Flipped Case: " + StringUtils.swapCase(userInput));
+
+        System.out.println("Reversed: " + StringUtils.reverse(userInput));
+
+
+
+
+
+//        String world = "world";
+//        System.out.println(StringUtils.prependIfMissing(world, "Hello, "));
     }
 }
